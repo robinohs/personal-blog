9bb322ab34006df351314fde662ab5ef0b0393ef
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-05-02 20:25:45 +0200

    Extract Sections to own files + remformat code a bit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { Anchor, Avatar, Box, Stack, Text } from "@mantine/core";
-import { ChevronRightIcon } from "@modulz/radix-icons";
 import Layout from "components/Layout";
 import Section from "components/Section";
 import type { NextPage } from "next";
@@ -110,244 +109,6 @@ const Intro = () => (
   </Section>
 );
 
-const ItemList = ({
-  label,
-  children,
-}: {
-  label: string;
-  children?: React.ReactNode;
-}) => (
-  <Box
-    sx={{
-      display: "flex",
-      flexDirection: "column",
-      width: "100%",
-      "@media (min-width: 800px)": {
-        alignContent: "flex-start",
-      },
-    }}
-  >
-    <Text
-      sx={{
-        width: "100%",
-        textAlign: "center",
-        fontWeight: "300",
-        fontSize: "25px",
-        paddingBottom: 2,
-        "@media (min-width: 800px)": {
-          textAlign: "left",
-        },
-      }}
-    >
-      {label}
-    </Text>
-    {children}
-  </Box>
-);
-
-const CustomListItem = ({
-  text,
-  experience,
-  href,
-}: {
-  text: string;
-  experience: string;
-  href: string;
-}) => (
-  <Box
-    sx={{
-      width: "100%",
-      display: "flex",
-      alignContent: "center",
-      justifyContent: "center",
-      "@media (min-width: 800px)": {
-        justifyContent: "flex-start",
-      },
-    }}
-  >
-    <Anchor
-      href={href}
-      target="_blank"
-      sx={{
-        ":hover": { textDecoration: "none" },
-      }}
-    >
-      <Box
-        sx={(theme) => ({
-          cursor: "pointer",
-          display: "flex",
-          alignItems: "center",
-          textAlign: "left",
-          justifyContent: "center",
-          padding: "7px 0",
-          borderRadius: 5,
-          ":hover": {
-            color:
-              theme.colorScheme === "dark"
-                ? theme.colors.blue[4]
-                : theme.colors.blue[2],
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.gray[9]
-                : theme.colors.gray[1],
-          },
-        })}
-      >
-        <Text
-          sx={(theme) => ({
-            width: "30px",
-            color:
-              theme.colorScheme === "dark"
-                ? theme.colors.blue[4]
-                : theme.colors.blue[6],
-          })}
-        >
-          <ChevronRightIcon />
-        </Text>
-        <Text
-          sx={(theme) => ({
-            width: "110px",
-            fontWeight: "bold",
-            color:
-              theme.colorScheme === "dark"
-                ? theme.colors.gray[0]
-                : theme.colors.gray[8],
-          })}
-        >
-          {text}
-        </Text>
-        <Text
-          sx={(theme) => ({
-            width: "80px",
-            fontWeight: "light",
-            fontSize: "14px",
-            color:
-              theme.colorScheme === "dark"
-                ? theme.colors.gray[5]
-                : theme.colors.gray[6],
-          })}
-        >
-          {experience}
-        </Text>
-      </Box>
-    </Anchor>
-  </Box>
-);
-
-const Technology = () => (
-  <Section title={"Technology."}>
-    <Text
-      sx={(theme) => ({
-        textAlign: "center",
-        color:
-          theme.colorScheme === "dark"
-            ? theme.colors.gray[5]
-            : theme.colors.gray[8],
-        "@media (min-width: 800px)": {
-          textAlign: "left",
-        },
-      })}
-    >
-      The following overview shows the technologies I currently use and those I
-      already gathered experience with:
-    </Text>
-    <Box
-      sx={(theme) => ({
-        width: "100%",
-        display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "center",
-        flexDirection: "column",
-        color:
-          theme.colorScheme === "dark"
-            ? theme.colors.gray[0]
-            : theme.colors.dark[6],
-        gap: 25,
-        "@media (min-width: 800px)": {
-          flexDirection: "row",
-        },
-      })}
-    >
-      <ItemList label="Frameworks / Libraries:">
-        <CustomListItem
-          text="Spring Boot"
-          experience="1 year +"
-          href="https://spring.io"
-        />
-        <CustomListItem
-          text="React.js"
-          experience="1 year +"
-          href="https://reactjs.org/"
-        />
-        <CustomListItem
-          text="Next.js"
-          experience="1/2 year +"
-          href="https://nextjs.org/"
-        />
-        <CustomListItem
-          text="Blitz.js"
-          experience="Basics"
-          href="https://blitzjs.com/"
-        />
-        <CustomListItem
-          text="Prisma.io"
-          experience="Basics"
-          href="https://www.prisma.io/"
-        />
-      </ItemList>
-      <ItemList label="Programming languages:">
-        <CustomListItem
-          text="Java"
-          experience="2 year +"
-          href="https://www.java.com/"
-        />
-        <CustomListItem
-          text="Typescript"
-          experience="1 year +"
-          href="https://www.typescriptlang.org/"
-        />
-        <CustomListItem
-          text="Python"
-          experience="Common"
-          href="https://www.python.org/"
-        />
-        <CustomListItem
-          text="C#"
-          experience="Basics"
-          href="https://dotnet.microsoft.com/en-us/"
-        />
-        <CustomListItem
-          text="C"
-          experience="Basics"
-          href="https://www.iso.org/standard/74528.html"
-        />
-      </ItemList>
-      <ItemList label="Other Tools / Software:">
-        <CustomListItem
-          text="Atlassian Jira"
-          experience="1 year"
-          href="https://www.atlassian.com/software/jira"
-        />
-        <CustomListItem
-          text="Atlassian Confluence"
-          experience="1 year"
-          href="https://www.atlassian.com/software/confluence"
-        />
-        <CustomListItem
-          text="Git"
-          experience="1 year+"
-          href="https://git-scm.com/"
-        />
-        <CustomListItem
-          text="Continuous Integration"
-          experience="Basics"
-          href="#"
-        />
-      </ItemList>
-    </Box>
-  </Section>
-);
-
 const SocialButton = ({
   icon,
   name,
@@ -392,9 +153,18 @@ const SocialButton = ({
 );
 
 const ContactMe = () => (
-  <Section title={"Stay in contact."}>
+  <Section
+    title={"Stay in contact."}
+    subtitle="Feel free to reach out to me on the following social platforms:"
+  >
     <Box
       sx={(theme) => ({
+        marginTop: "35px",
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+        gap: 25,
         textAlign: "left",
         color:
           theme.colorScheme === "dark"
@@ -402,53 +172,31 @@ const ContactMe = () => (
             : theme.colors.gray[8],
       })}
     >
-      <Text
-        sx={{
-          textAlign: "center",
-          marginBottom: 25,
-          "@media (min-width: 800px)": {
-            flexDirection: "row",
-            textAlign: "left",
-          },
-        }}
-      >
-        Feel free to reach out to me on the following social platforms:
-      </Text>
-      <Box
-        sx={{
-          display: "flex",
-          width: "100%",
-          justifyContent: "space-around",
-          flexWrap: "wrap",
-          gap: 25,
-        }}
-      >
-        <SocialButton
-          icon="bi bi-linkedin"
-          name="Linkedin"
-          href="https://www.linkedin.com/in/robin-ohs-b6b4051a1/?locale=en_US"
-        />
-        <SocialButton
-          icon="bi bi-github"
-          name="GitHub"
-          href="https://github.com/Roboh97"
-        />
-        <SocialButton
-          icon="bi bi-instagram"
-          name="Instagram"
-          href="https://www.instagram.com/robin.ohs/"
-        />
-        <SocialButton
-          icon="bi bi-twitter"
-          name="Twitter"
-          href="https://twitter.com/roboh97"
-        />
-        <SocialButton
-          icon="bi bi-stack-overflow"
-          name="StackOverflow"
-          href="https://stackoverflow.com/users/15174092/roboh97"
-        />
-      </Box>
+      <SocialButton
+        icon="bi bi-linkedin"
+        name="Linkedin"
+        href="https://www.linkedin.com/in/robin-ohs-b6b4051a1/?locale=en_US"
+      />
+      <SocialButton
+        icon="bi bi-github"
+        name="GitHub"
+        href="https://github.com/Roboh97"
+      />
+      <SocialButton
+        icon="bi bi-instagram"
+        name="Instagram"
+        href="https://www.instagram.com/robin.ohs/"
+      />
+      <SocialButton
+        icon="bi bi-twitter"
+        name="Twitter"
+        href="https://twitter.com/roboh97"
+      />
+      <SocialButton
+        icon="bi bi-stack-overflow"
+        name="StackOverflow"
+        href="https://stackoverflow.com/users/15174092/roboh97"
+      />
     </Box>
   </Section>
 );
@@ -457,7 +205,6 @@ const Home: NextPage = () => (
   <Layout title="Home" enableBreadcrumb>
     <Stack spacing={50}>
       <Intro />
-      <Technology />
       <ContactMe />
     </Stack>
   </Layout>
---
+++ pages/use.tsx
@@ -0,0 +1,11 @@
+import Layout from "components/Layout";
+import { GearSection, TechnologySection } from "core/sections";
+
+const Use = () => (
+  <Layout title="Use" enableBreadcrumb>
+    <TechnologySection />
+    <GearSection />
+  </Layout>
+);
+
+export default Use;
