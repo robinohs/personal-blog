be23f1727cd7afd0ccb8b33206c73087826ed4c6
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-05-02 19:26:48 +0200

    Setup testing for logic methods.

---
+++ types/BreadcrumbItem.type.ts
@@ -0,0 +1,4 @@
+export default interface BreadcrumbItem {
+  url: string;
+  name: string;
+}
---
+++ utils/createBreadcrumb.ts
@@ -0,0 +1,36 @@
+import BreadcrumbItem from "types/BreadcrumbItem.type";
+
+/**
+ * Takes a raw path and creates an array of BreadcrumbItems for it.
+ * @param rawPath the rawPath given from router.
+ * @returns the created BreadcrumbItems for this path.
+ */
+const createBreadcrumb = (rawPath: string): BreadcrumbItem[] => {
+  const path = clearPathFromFragment(rawPath);
+  const paths = path === "/" ? [""] : path.split("/");
+  return paths.map((path, index) => createItemForPath(paths, path, index));
+};
+
+const createItemForPath = (
+  paths: string[],
+  path: string,
+  index: number
+): BreadcrumbItem => ({
+  name: path === "" ? "Home" : path.charAt(0).toUpperCase() + path.slice(1),
+  url: paths.reduce((old, curr, i) => {
+    if (i > index) return `${old}`;
+    if (old === "/") return `/${curr}`;
+    return `${old}/${curr}`;
+  }, "/"),
+});
+
+/**
+ * Clears a path from fragment.
+ * @param path the path to clear
+ * @returns the path without the fragment.
+ */
+const clearPathFromFragment = (path: string): string => {
+  return path.split("#", 1)[0];
+};
+
+export default createBreadcrumb;
