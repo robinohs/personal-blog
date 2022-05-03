d0f3224b79476d5bf4085fc7d60075d7fefca789
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-05-03 19:18:55 +0200

    Add pagination to list.

--- core/sections/blog/ArticlesSection.tsx
+++ core/sections/blog/ArticlesSection.tsx
@@ -1,22 +1,40 @@
-import { Stack, Text } from "@mantine/core";
+import { Pagination, Stack, Text } from "@mantine/core";
 import ArticleItem from "core/components/ArticleItem";
 import SearchBar from "core/components/SearchBar";
 import Section from "core/components/Section";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { PostMetaData } from "types/Post.type";
 import filterPosts from "utils/filterPosts";
+
+const ARTICLES_PER_PAGE = 20;
 
 type ArticlesSectionProps = {
   posts: PostMetaData[];
 };
 const ArticlesSection = ({ posts }: ArticlesSectionProps) => {
+  const [activePage, setPage] = useState(1);
   const [query, setQuery] = useState("");
   const [shownPosts, setShownPosts] = useState<PostMetaData[]>([]);
+  const [paginatePosts, setPaginatePosts] = useState<PostMetaData[]>([]);
+
+  const MAX_PAGES = useMemo(
+    () => Math.ceil(shownPosts.length / ARTICLES_PER_PAGE),
+    [shownPosts]
+  );
 
   useEffect(() => {
     const filteredPosts = filterPosts(query, posts);
     setShownPosts(filteredPosts);
+    setPage(1);
   }, [posts, query]);
+
+  useEffect(() => {
+    const showPosts = shownPosts.slice(
+      (activePage - 1) * ARTICLES_PER_PAGE,
+      (activePage - 1) * ARTICLES_PER_PAGE + ARTICLES_PER_PAGE
+    );
+    setPaginatePosts(showPosts);
+  }, [shownPosts, activePage]);
 
   return (
     <Section
@@ -25,7 +43,7 @@ const ArticlesSection = ({ posts }: ArticlesSectionProps) => {
     >
       <SearchBar handler={setQuery} />
       <Stack sx={{ marginTop: "25px" }}>
-        {shownPosts.map((post) => (
+        {paginatePosts.map((post) => (
           <ArticleItem key={post.slug} post={post} />
         ))}
         {shownPosts.length === 0 && query === "" && (
@@ -35,6 +53,11 @@ const ArticlesSection = ({ posts }: ArticlesSectionProps) => {
           <Text>No articles found matching query `{query}`.</Text>
         )}
       </Stack>
+      {shownPosts.length > 0 && (
+        <Stack align="center" sx={{ marginTop: "55px" }}>
+          <Pagination page={activePage} onChange={setPage} total={MAX_PAGES} />
+        </Stack>
+      )}
     </Section>
   );
 };
