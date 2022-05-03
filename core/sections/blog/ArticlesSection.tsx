2326f04f8e24ce097619f2cb12f3a3f06ad9f6bb
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-05-03 18:50:07 +0200

    Make posts filterable by searchbar.

--- core/sections/blog/ArticlesSection.tsx
+++ core/sections/blog/ArticlesSection.tsx
@@ -1,25 +1,39 @@
-import { Stack, TextInput } from "@mantine/core";
-import { MagnifyingGlassIcon } from "@modulz/radix-icons";
+import { Stack, Text } from "@mantine/core";
 import ArticleItem from "core/components/ArticleItem";
+import SearchBar from "core/components/SearchBar";
 import Section from "core/components/Section";
+import { useEffect, useState } from "react";
 import { PostMetaData } from "types/Post.type";
+import filterPosts from "utils/filterPosts";
 
 type ArticlesSectionProps = {
   posts: PostMetaData[];
 };
 const ArticlesSection = ({ posts }: ArticlesSectionProps) => {
-  const searchChanged = (newSearch: string) => {};
+  const [query, setQuery] = useState("");
+  const [shownPosts, setShownPosts] = useState<PostMetaData[]>([]);
+
+  useEffect(() => {
+    const filteredPosts = filterPosts(query, posts);
+    setShownPosts(filteredPosts);
+  }, [posts, query]);
 
   return (
     <Section
       title="Blog."
       subtitle="These are my newest blog articles. I love to write about the newest frameworks and technologies I'm using. You can use the search below to filter articles by title or tags."
     >
-      <TextInput placeholder="Your search..." icon={<MagnifyingGlassIcon />} />
+      <SearchBar handler={setQuery} />
       <Stack sx={{ marginTop: "25px" }}>
-        {posts.map((post) => (
+        {shownPosts.map((post) => (
           <ArticleItem key={post.slug} post={post} />
         ))}
+        {shownPosts.length === 0 && query === "" && (
+          <Text>No articles found.</Text>
+        )}
+        {shownPosts.length === 0 && query !== "" && (
+          <Text>No articles found matching query `{query}`.</Text>
+        )}
       </Stack>
     </Section>
   );
