24bbbed60f62ccdcaf9be460bf46d447eed46cc0
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-05-03 13:41:27 +0200

    Blog article detail page.

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,64 @@
+import { Title, TypographyStylesProvider } from "@mantine/core";
+import Layout from "core/components/Layout";
+import YouTube from "core/components/YouTube";
+import { getPostFromSlug, getSlugs } from "core/src/PostApi";
+import "highlight.js/styles/atom-one-dark.css";
+import type { GetStaticPaths, GetStaticProps } from "next";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import Image from "next/image";
+import rehypeAutoLinkHeadings from "rehype-autolink-headings";
+import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
+import type { PostMetaData } from "types/Post.type";
+
+type PostMDX = {
+  meta: PostMetaData;
+  source: MDXRemoteSerializeResult<Record<string, unknown>>;
+};
+
+const BlogDetailPage = ({ source, meta }: PostMDX) => {
+  return (
+    <Layout title={meta.title} enableBreadcrumb>
+      <Title order={1}>{meta.title}</Title>
+      <TypographyStylesProvider>
+        <MDXRemote {...source} components={{ YouTube, Image }} />
+      </TypographyStylesProvider>
+    </Layout>
+  );
+};
+
+export default BlogDetailPage;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params as { slug: string };
+  const { content, meta } = getPostFromSlug(slug);
+
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutoLinkHeadings, { behavior: "wrap" }],
+        rehypeHighlight,
+      ],
+    },
+  });
+
+  return {
+    props: {
+      meta: meta,
+      source: mdxSource,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getSlugs().map((slug) => ({
+    params: { slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
