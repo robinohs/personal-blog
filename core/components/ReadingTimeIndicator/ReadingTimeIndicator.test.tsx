11458e55aba1b023c1ac93b6875a0c4f6576e624
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-05-16 12:17:18 +0200

    Add TestUtilities to generate random strings and words

--- core/components/ReadingTimeIndicator/ReadingTimeIndicator.test.tsx
+++ core/components/ReadingTimeIndicator/ReadingTimeIndicator.test.tsx
@@ -1,60 +1,29 @@
 import { render, screen } from "@testing-library/react";
+import { generateStringWithGivenCountOfWords } from "@utils/testUtilities";
 import ReadingTimeIndicator from "./ReadingTimeIndicator";
-
-const fiftyWordsLongText =
-  "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1";
 
 describe("Welcome component", () => {
   it("Short text(<200) has 1 minute reading time.", () => {
-    render(<ReadingTimeIndicator text={fiftyWordsLongText} />);
+    render(
+      <ReadingTimeIndicator text={generateStringWithGivenCountOfWords(50)} />
+    );
     expect(screen.getByText("1 minutes to read")).toBeDefined();
   });
   it("Long text(=200) has 1 minute reading time.", () => {
     render(
-      <ReadingTimeIndicator
-        text={
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText
-        }
-      />
+      <ReadingTimeIndicator text={generateStringWithGivenCountOfWords(200)} />
     );
     expect(screen.getByText("1 minutes to read")).toBeDefined();
   });
   it("Long text(=400) has 2 minute reading time.", () => {
     render(
-      <ReadingTimeIndicator
-        text={
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText
-        }
-      />
+      <ReadingTimeIndicator text={generateStringWithGivenCountOfWords(400)} />
     );
     expect(screen.getByText("2 minutes to read")).toBeDefined();
   });
-  it("Long text(=300) has 3 minute reading time.", () => {
+  it("Long text(=500) has 3 minute reading time.", () => {
     render(
-      <ReadingTimeIndicator
-        text={
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText +
-          fiftyWordsLongText
-        }
-      />
+      <ReadingTimeIndicator text={generateStringWithGivenCountOfWords(500)} />
     );
     expect(screen.getByText("3 minutes to read")).toBeDefined();
   });
