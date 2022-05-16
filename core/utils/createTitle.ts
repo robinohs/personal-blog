dadffe24c83b1b77377731f4ea97b33d3e06035b
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-05-16 12:16:32 +0200

    Switch imports to path resolution + add some jsdoc

--- core/utils/createTitle.ts
+++ core/utils/createTitle.ts
@@ -1,3 +1,9 @@
+/**
+ * Creates the page title by inserting a text in the given placeholder.
+ *
+ * @param title the text to insert in the placeholder.
+ * @returns the created title.
+ */
 const createTitle = (title: String | undefined) =>
   `Blog@Roboh97${title ? ` - ${title}` : ""}`;
 
