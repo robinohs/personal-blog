8dea9548c218a1e0a6fc2f94405f3c11d56e0231
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-06-25 18:13:39 +0200

    New ProjectCard component.

---
+++ core/components/ProjectCard/ProjectCard.style.ts
@@ -0,0 +1,24 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  group: { marginBottom: 5, marginTop: theme.spacing.sm },
+  text: {
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[1]
+        : theme.colors.gray[7],
+    lineHeight: 1.5,
+    marginTop: theme.spacing.sm,
+  },
+  listLeft: {
+    minWidth: "100px",
+  },
+  listBade: {
+    width: "90px",
+  },
+  image: {
+    marginTop: theme.spacing.md,
+  }
+}));
+
+export default useStyles;
