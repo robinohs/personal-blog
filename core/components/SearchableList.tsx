3c2f99ad63088bc7d97dbd1c354b44daaecf54bc
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-07-02 15:58:44 +0200

    Add optional handler to get query in parent component.

--- core/components/SearchableList.tsx
+++ core/components/SearchableList.tsx
@@ -1,24 +1,33 @@
 import { Box } from "@mantine/core";
-import { ReactNode, useEffect, useState } from "react";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import SearchBar from "./SearchBar";
 
 type Props<T> = {
   searchFilter: (query: string, items: T[]) => T[];
   items: T[];
   children: (items: T[]) => ReactNode;
+  getQueryHandler?: Dispatch<SetStateAction<string>>;
 };
 const SearchableList = <T extends Object>({
   searchFilter,
   items,
   children,
+  getQueryHandler,
 }: Props<T>) => {
   const [query, setQuery] = useState("");
   const [shownItems, setShownItems] = useState<T[]>(items);
 
   useEffect(() => {
+    getQueryHandler && getQueryHandler(query);
     const filteredItems = searchFilter(query, items);
     setShownItems(filteredItems);
-  }, [items, searchFilter, query]);
+  }, [getQueryHandler, items, searchFilter, query]);
 
   return (
     <Box>
